7ff205255f20d7968b3fb73636f90ed91f05b191
Alex <goldberg.alex@gmail.com>
2021-07-22 19:10:50 +0300

    changed problem to srr problem

--- generalzed_Kmeans/GMM_Params.py
+++ generalzed_Kmeans/GMM_Params.py
@@ -1,7 +1,8 @@
 
 
-number_of_clusters = 8
+number_of_clusters = 4
 number_of_modes = 10
-side_length_factor = 2
+side_length = 15
+side_width = 20
 mean_to_variance_ratio = 5
 ellpsoid_ratio = 10
--- generalzed_Kmeans/main.py
+++ generalzed_Kmeans/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.linalg as spla
+import shapely.geometry as shapes
 from matplotlib.patches import Rectangle
 from scipy.stats import ortho_group
 from sklearn import mixture
@@ -8,8 +9,8 @@ from sklearn import mixture
 import GMM_Params
 from BoundingRect import BoundingRect
 
-def shift_to_positive_orthant(vector):
 
+def shift_to_positive_orthant(vector):
     if np.sign(vector[0]) != np.sign(vector[1]):
         vector[0] = np.abs(vector[0])
         vector[1] = -1 * np.abs(vector[1])
@@ -38,9 +39,27 @@ def generate_dist(covMat, mean):
     return noisy_vector
 
 
+def create_polygon_samples():
+    polygon = shapes.Polygon([(0, 0), (50, 0), (50, 50), (0, 0)])
+    samples = []
+    # generate samples inside the polygon with rejection sampling
+    while len(samples) < 10000:
+        coords = np.random.uniform(0, 50, (2))
+        point = shapes.Point(coords)
+        if polygon.contains(point):
+            samples.append(coords)
+
+    samples = np.array(samples)
+    plt.figure()
+    plt.scatter(samples[:, 0], samples[:, 1], s=5)
+
+    return samples
+
+
 def generate_dataset(number_of_clusters, mean_to_variance_ratio):
     plt.figure()
     dataset = None
+
     for idx in range(number_of_clusters):
 
         # sample random covariance / mean
@@ -70,8 +89,8 @@ def random_PSD():
 
 def main():
     # Generate ellipsoid blobs:
-    dataset = generate_dataset(GMM_Params.number_of_modes, GMM_Params.mean_to_variance_ratio)
-
+    # dataset = generate_dataset(GMM_Params.number_of_modes, GMM_Params.mean_to_variance_ratio)
+    dataset = create_polygon_samples()
     # generate Gaussian mixture that approximates uniform distribution on dataset
     centers = np.zeros((GMM_Params.number_of_clusters, 2))
     gmm = mixture.GaussianMixture(n_components=GMM_Params.number_of_clusters, covariance_type='full').fit(dataset)
@@ -89,16 +108,15 @@ def print_srr(dataset, model_rect):
     ax.scatter(dataset[:, 0], dataset[:, 1], s=50, cmap='viridis')
     for idx in range(GMM_Params.number_of_clusters):
         ax.add_patch(Rectangle(model_rect[idx].anchor,
-                               width=2 * model_rect[idx].width,
-                               height=2 * model_rect[idx].height,
-                               angle=model_rect[idx].angle * 180 / np.pi,
+                               width=model_rect[idx].width,
+                               height=model_rect[idx].height,
+                               angle=np.rad2deg(model_rect[idx].angle),
                                fill=False,
                                linewidth=2))
     plt.show()
 
 
 def generate_bounding_rect(covariance, mean):
-
     # compute spectral decomposition
     eigenvalues, eigenvectors = np.linalg.eig(covariance)
 
@@ -108,8 +126,8 @@ def generate_bounding_rect(covariance, mean):
 
     # compute rectangle attributes
     ellipsoid_angle = np.arctan2(-eigenvectors[0, 0], eigenvectors[1, 0])
-    width = GMM_Params.side_length_factor * np.sqrt(eigenvalues[1])
-    height = GMM_Params.side_length_factor * np.sqrt(eigenvalues[0])
+    width = GMM_Params.side_length  # * np.sqrt(eigenvalues[1])
+    height = GMM_Params.side_width  # * np.sqrt(eigenvalues[0])
     w_shift = width * np.array([eigenvectors[0, 1], eigenvectors[1, 1]])
     h_shift = height * np.array([eigenvectors[0, 0], eigenvectors[1, 0]])
 
@@ -124,7 +142,6 @@ def generate_bounding_rect(covariance, mean):
 
 if __name__ == "__main__":
     main()
-
 
 #######################  JUNK YARD #####################################
 #
